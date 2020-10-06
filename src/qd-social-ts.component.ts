47218ce4a06e9b09814218e0625862f242fab010
StationedInTheField <ask@stationedinthefield.com>
2020-10-06 07:54:27 -0700

    stream switch

--- src/qd-social-ts.component.ts
+++ src/qd-social-ts.component.ts
@@ -20,5 +20,8 @@ export class QDSocialComponent {
   }
 
   selectedProfile = "NoProfileSelected";
-
+  streamsActive = false;
+  toggleStreams(){
+    this.streamsActive = !this.streamsActive;
+  }
 }
